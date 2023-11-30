64c21d4b212a15b409e829d63c6de9959ff8c399
Ethan <q40131125@gmail.com>
2023-11-30 17:17:23 +0800

    Add all the content of the app

--- 貓趣世界/ViewController.swift
+++ 貓趣世界/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "因為你點開了，請打賞罐頭！", message: "喵喵喵喵喵喵喵喵喵喵", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 
 }
